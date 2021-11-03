c42230f8da66a6bc55860de87dca7469b2fb3a9d
Andy Lindsay <andydlindsay@gmail.com>
2021-11-03 15:53:41 -0700

    more updates

--- w02d04/helpers/promise-generator.js
+++ w02d04/helpers/promise-generator.js
@@ -1,6 +1,9 @@
-module.exports = (delay = 5000, isRandom = false) => {
+const generatePromise = (name, delay = 3000, isRandom = false) => {
   const wait = isRandom ? Math.floor(Math.random() * delay) : delay;
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, wait);
+
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(name), wait);
   });
 };
+
+module.exports = generatePromise;
